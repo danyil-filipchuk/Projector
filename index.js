b3dcc85758b0370e7d8e4469200c96eff86140c5
danyil-filipchuk <filipchuk.kyiv@gmail.com>
2024-11-12 22:25:08 +0200

    First homework

---
+++ index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// task 1
+
+let a = 10;
+let b = 5;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a ** b);
+console.log(a % b);
+
+// task 2
+
+let info1 = 5;
+info1 = 'Projector';
+
+console.log(info1);
+
+let info2 = 'Institute';
+info2 = 10;
+
+console.log(info2);
+
+const number = 5;
+
+console.log(Boolean(number));
