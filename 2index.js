2f09b083cacdfb498c12cd506ad75569595dfd12
danyil-filipchuk <filipchuk.kyiv@gmail.com>
2024-11-24 16:31:15 +0200

    changes

--- 2index.js
+++ 2index.js
@@ -3,20 +3,12 @@
 // task 1 (part 1):
 
 for (let i = 1; i <= 100; i++) {
-    if (i % 3 == 0) {
-        console.log('Лол')
-    } else if (i % 5 == 0) {
-        console.log('Кек')
-    } else {
-        console.log(i);
-    }
-}
-
-// task 1 (part 2):
-
-for (let i = 1; i <= 100; i++) {
-    if (i % 3 == 0 || i % 5 == 0) {
+    if ((i % 3 === 0) && (i % 5 === 0)) {
         console.log('ЛолКек')
+    } else if (i % 3 === 0) {
+        console.log('Лол')
+    } else if (i % 5 === 0) {
+        console.log('Кек')
     } else {
         console.log(i);
     }
@@ -24,9 +16,15 @@ for (let i = 1; i <= 100; i++) {
 
 // task 2 (part 1):
 
-for (value = 1; value < 10; value++) {
-    if (value % 2 == 0) {
-        console.log(value);
+let value = 1;
+
+if((typeof value !== "number") || (isNaN(value))){
+    console.log('Таке чуство шо Бог десь наказує нас за шось');
+} else {
+    for (; value < 10; value++) {
+        if (value % 2 === 0) {
+            console.log(value);
+        }
     }
 }
 
@@ -34,22 +32,11 @@ for (value = 1; value < 10; value++) {
 
 let value = 1;
 
-while(value < 10) {
-    if(value % 2 == 0) {
-        console.log(value);
-    }
-    value++
-}
-
-// task 2 (part 3):
-
-let value = '1';
-
-if(typeof value !== "number") {
+if((typeof value !== "number") || (isNaN(value))) {
     console.log('Таке чуство шо Бог десь наказує нас за шось');
 } else {
     while (value < 10) {
-        if (value % 2 == 0) {
+        if (value % 2 === 0) {
             console.log(value);
         }
         value++
