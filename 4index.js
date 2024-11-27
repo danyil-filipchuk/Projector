178d39f5704248a1eabc7fcf9b4eae9c82e688e1
danyil-filipchuk <filipchuk.kyiv@gmail.com>
2024-11-27 23:50:01 +0200

    arrays change

--- 4index.js
+++ 4index.js
@@ -1,4 +1,29 @@
-'use string';
+'use strict';
+
+// task 2 (first option):
+
+const userNames = ['Петро', 'Емма', 'Юстин', 'Ілля', 'Марта', 'Яна', 'Василь', 'Антон', 'Олена'];
+
+let filteredNames = userNames.filter( item => {
+    if(['А','Е','И','І','Ї','О','У','Ю','Я','Є'].includes(item[0])) {
+        return true;
+    }
+})
+
+console.log(filteredNames);
+
+// task 2 (second option):
+
+const userNames = ['Петро', 'Емма', 'Юстин', 'Ілля', 'Марта', 'Яна', 'Василь', 'Антон', 'Олена'];
+const filteredNames = [];
+
+for(let i = 0; i < userNames.length; i++) {
+    let arr = userNames[i];
+    if(['А','Е','И','І','Ї','О','У','Ю','Я'].includes(arr[0])) {
+        filteredNames.push(arr);
+    }
+}
+console.log(filteredNames);
 
 // task 3 (first option):
 
@@ -30,18 +55,6 @@ let productOfArray = resultsArray2.reduce((accum, item) => {
 
 console.log(productOfArray); // 24
 
-// task 4 (second option):
-
-const resultsArray = [1, 2, [3, [4]]];
-
-const resultsArray2 = resultsArray.flat(Infinity);
-
-let productOfArray = resultsArray2.reduce(function(accum, item) {
-    return accum * item
-})
-
-console.log(productOfArray); // 24
-
-
-
-
+// or: let productOfArray = resultsArray2.reduce(function(accum, item) {
+//     return accum * item
+// })
