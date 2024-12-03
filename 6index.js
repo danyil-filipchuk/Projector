df52ab2451428c8327f178c4efdf20138906e6ca
danyil-filipchuk <filipchuk.kyiv@gmail.com>
2024-12-03 15:31:10 +0200

    hw6 first commit

---
+++ 6index.js
@@ -0,0 +1,43 @@
+'use strict'
+
+/*
+
+// task 1 (але поки ще не вірно зроблено):
+
+function addThemAll (...arr) {
+
+    const sum = arr.reduce((accum, item) => {
+        return accum + item;
+    })
+    return sum;
+}
+
+console.log(addThemAll(2,4)); // 6
+console.log(addThemAll(1,2,3,4)); // 10
+console.log(addThemAll(5,5,10)); // 20
+
+// task 2:
+
+function multiply(a) {
+    return function calculate(b) {
+        return a * b;
+    }
+}
+
+console.log(multiply(5)(5))	// 25
+console.log(multiply(2)(-2))  // -4
+console.log(multiply(4)(3))	// 12
+
+// task 4:
+
+const userNames = ['Петро', 'Емма', 'Петро', 'Емма', 'Марта', 'Яна', 'Василь', 'Антон', 'Олена', 'Емма'];
+
+function filterUnique(array) {
+
+    return [...new Set(array)];
+
+}
+
+console.log(filterUnique(userNames)); // ['Петро', 'Емма', 'Марта', 'Яна', 'Василь', 'Антон', 'Олена'];
+
+ */
