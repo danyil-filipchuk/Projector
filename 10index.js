dd516001d412f1a76b205709622763c2c07995d0
danyil-filipchuk <filipchuk.kyiv@gmail.com>
2025-01-12 00:13:40 +0200

    final commit

--- 10index.js
+++ 10index.js
@@ -8,9 +8,10 @@ const form = document.querySelector('.create-task-form');
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     if (taskInput.value.trim() === '') {
-        return;
+        return alert('Введіть текст задачі')
     }
 
+    // Створення змінної-об'єкта з двома ключами - айді та самим текстом.
     const task = {
         id: generateId(),
         text: taskInput.value.trim(),
@@ -21,6 +22,7 @@ form.addEventListener('submit', (event) => {
     taskInput.value = '';
 })
 
+// Функція генерації унікального айді нашим елементам.
 function generateId() {
     return Date.now();
 }
@@ -29,33 +31,44 @@ function createSingleTaskElement(taskInput) {
     const li = document.createElement('li');
     li.appendChild(document.createTextNode(taskInput.text));
     li.classList.add('item-task');
+    // Створення атрибуту та присвоєння йому значення.
     li.setAttribute('data-id', taskInput.id);
 
+
+    // Створення кнопки після тексту завдання.
     const doneTask = document.createElement('button');
     doneTask.textContent = 'Виконано';
     doneTask.classList.add('done-task');
 
+    // Створення іконки редагування після кнопки.
     const editTask = document.createElement('i');
     editTask.classList.add('fa', 'fa-edit', 'edit-task', 'fa-lg');
 
+    //Додаю кнопку та іконку до "li".
     li.appendChild(doneTask);
     li.appendChild(editTask);
+
+    //Додаю весь блок "li" разом з текстом завдання, кнопкою та іконкою до нашої колекції.
     tasksList.appendChild(li);
 
+    // Подія видалення завдання разом з викликом функції.
     doneTask.addEventListener('click', () => {
         removeFromLocalStorage(taskInput.id);
         li.remove();
-    })
+    });
 
+    // Подія редагування тексту завдання разом з викликом функції.
     editTask.addEventListener('click', () => {
         const newText = prompt('Виправіть текст завдання', taskInput.text);
         if (newText && newText.trim() !== '') {
             taskInput.text = newText.trim();
             li.firstChild.textContent = taskInput.text;
+            editTaskInLocalStorage(taskInput);
         }
-    })
+    });
 }
 
+// Тут все без змін, не чіпав.
 function storeTaskInLocalStorage(task) {
     const tasks = localStorage.getItem('tasks') !== null
         ? JSON.parse(localStorage.getItem('tasks')) : [];
@@ -63,6 +76,8 @@ function storeTaskInLocalStorage(task) {
     localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
+// Функція видалення завдання з LocalStorage, за допомогою методу filter.
+// Тобто в змінній filterTasks залишаються всі завдання крім тих, що нам не потрібні по айді.
 function removeFromLocalStorage(taskId) {
     const tasks = localStorage.getItem('tasks') !== null
     ? JSON.parse(localStorage.getItem('tasks')) : [];
@@ -71,11 +86,24 @@ function removeFromLocalStorage(taskId) {
     localStorage.setItem('tasks', JSON.stringify(filterTasks));
 }
 
+// Функція зміни тексту завдання. Спробував скористатись тернарним оператором замість логічної конструкції.
+function editTaskInLocalStorage(editedTask) {
+    const tasks = localStorage.getItem('tasks') !== null
+    ? JSON.parse(localStorage.getItem('tasks')) : [];
+
+    const editTask = tasks.map(task =>
+        task.id === editedTask.id ? editedTask : task
+    );
+    localStorage.setItem('tasks', JSON.stringify(editTask));
+}
+
+// Видалення всіх елементів. В умовах завдання цього не було, але в шаблоні воркшопу було.
 clearBtn.addEventListener('click', () => {
     tasksList.innerHTML = '';
     localStorage.removeItem('tasks');
 });
 
+// Тут все без змін, не чіпав.
 document.addEventListener('DOMContentLoaded', () => {
     const tasks = localStorage.getItem('tasks') !== null
         ? JSON.parse(localStorage.getItem('tasks')) : [];
